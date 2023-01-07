52a2edd0bf169e0792e871db01fd97dced69fe8a
Ansh Goyal <anshgoyal1704@gmail.com>
2023-01-07 22:32:37 +0530

    Change homepage image

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ passport.deserializeUser(function (id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/keepclone",
+    callbackURL: "http://localhost:3000/auth/google/linkshortener",
     userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
     passReqToCallback: true
 },
